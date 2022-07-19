5e3d38956d8de2825edffb507b695e4f16b43c3a
Dave Rothfarb <dave.rothfarb.webdev@gmail.com>
2022-07-19 11:56:26 -0400

    Added dbQueryer test stub. Will fill it out more once the DT data is completely restructured

---
+++ src/services/dbqueryer.test.js
@@ -0,0 +1,8 @@
+const firebase = require('firebase/app');
+const firestore = require('firebase/firestore');
+const firebaseConfig = require('../firebase.config.js');
+const dbQueryer = require('./dbqueryer');
+
+it('Should do something', () => {
+});
+
